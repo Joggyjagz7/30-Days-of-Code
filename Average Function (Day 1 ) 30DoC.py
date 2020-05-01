46736e3b362c363c7c49eb32f2e3ab5eb40cd2d6
Ejiro Onose <32671860+Joggyjagz7@users.noreply.github.com>
2020-05-01 23:39:40 +0100

    Average function
    30 Days of Code Python Intermediate task solution submission

---
+++ Average Function (Day 1 ) 30DoC.py
@@ -0,0 +1,9 @@
+# Python program to get average of a list 
+def Average(s): 
+    return sum(s) / len(s) 
+  
+s = [12,11,2,6,7,10] 
+average = Average(s) 
+  
+
+print("Average of the array s =", round(average, 2)) 
